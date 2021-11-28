f246129dab4a4dc445d9ecbcc090e72ea15564ee
pine0772 <pine0772@gmail.com>
2021-11-28 17:22:53 +0000

    merge

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -15,14 +16,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private:
+    void new_msg(QString text);
 private slots:
     void on_actionConfig_triggered();
-
     void on_actionMap_triggered();
-
     void on_VolumeSliderR_sliderMoved(int position);
-
     void on_VolumeSliderL_sliderMoved(int position);
+    void on_sentMessage();
+
+
 
 private:
     Ui::MainWindow *ui;
