9f133069c2da9369ab4434dbe04c48681d04d1d6
pine0772 <pine0772@gmail.com>
2021-12-11 23:05:05 +0000

    Changes to UI (Actions) Actions in menuTabs now can add/remove tabs Changed a few more tabs to work with resizing

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -29,10 +29,22 @@ private slots:
     void on_VolumeSliderR_sliderMoved(int position);
     void on_VolumeSliderL_sliderMoved(int position);
     void on_sentMessage();
-
+    void on_menuTabs_triggered();
 
 
     void on_NetConnectButton_clicked();
+
+    void on_actionComms_triggered();
+
+    void on_actionNet_triggered();
+
+    void on_actionData_triggered();
+
+    void on_actionDocuments_triggered();
+
+    void on_actionFeed_triggered();
+
+    void on_actionUsers_triggered();
 
 private:
     Ui::MainWindow *ui;
